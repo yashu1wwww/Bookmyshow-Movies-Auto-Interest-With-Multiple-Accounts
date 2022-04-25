0cf0cb910b49526a6e0e7148608e594446501024
Yashu_King11 <89150253+yashu1wwww@users.noreply.github.com>
2022-04-25 10:40:19 +0530

    Add files via upload

---
+++ bms.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get("https://in.bookmyshow.com/bengaluru/movies/kranti/ET00323056")
+
+input=driver.find_element_by_xpath('//*[@id="wzrk-confirm"]')
+input.click()
+
+input=driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section/div[2]/div[2]/button/span')
+input.click()
+
+input=driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div')
+input.click()
+
+input=driver.find_element_by_xpath('//*[@id="emailId"]')
+input.send_keys("yashuvirat08@gmail.com")
+
+input=driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button')
+input.click()
+
+
+
+
+
