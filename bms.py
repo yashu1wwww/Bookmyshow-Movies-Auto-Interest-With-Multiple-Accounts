e2bd0a0d9e4eb330c95c22d9981fcaaa678ad408
Yashu_Kohli11 <89150253+yashu1wwww@users.noreply.github.com>
2022-05-11 22:29:43 +0530

    Update bms.py

--- bms.py
+++ bms.py
@@ -15,7 +15,7 @@ input.send_keys("vkdhoni123@gmail.com")
 input=driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button')
 input.click()
 
-
+#In Last It Ask Select City Select One City 👍
 
 
 
