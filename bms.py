8fa06d1ce3e38d2a04d6a9b6f092328611d64c81
Yashu_Kohli11 <89150253+yashu1wwww@users.noreply.github.com>
2022-06-01 05:53:24 +0530

    Update bms.py

--- bms.py
+++ bms.py
@@ -3,17 +3,14 @@ from selenium import webdriver
 driver = webdriver.Chrome()
 driver.get("https://in.bookmyshow.com/login/?referer=/bengaluru/movies/kranti/ET00323056")
 
-input=driver.find_element_by_xpath('//*[@id="wzrk-confirm"]')
-input.click()
+input=driver.find_element_by_xpath('//*[@id="wzrk-confirm"]').click()
 
-input=driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div')
-input.click()
+input=driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div').click()
 
 input=driver.find_element_by_xpath('//*[@id="emailId"]')
 input.send_keys("vkdhoni123@gmail.com") #Replace Your Mail OTP Will Not Be Entered Automatically You Must Enter That
 
-input=driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button')
-input.click()
+input=driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button').click()
 
 #In Last It Ask Select City Select it👍
 
