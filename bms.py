22dd94104c85b33b20a2ef61f445dfd9707ab0a5
Yashu_Kohli11 <89150253+yashu1wwww@users.noreply.github.com>
2022-06-05 10:53:56 +0530

    Update bms.py

--- bms.py
+++ bms.py
@@ -1,18 +1,24 @@
 from selenium import webdriver
+import time
 
 driver = webdriver.Chrome()
-driver.get("https://in.bookmyshow.com/login/?referer=/bengaluru/movies/kranti/ET00323056")
+driver.get("https://in.bookmyshow.com/bengaluru/movies/kranti/ET00323056")
 
 input=driver.find_element_by_xpath('//*[@id="wzrk-confirm"]').click()
+
+input=driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section/div[2]/div[2]/button/span').click()
+
+#time.sleep(4)
 
 input=driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div').click()
 
 input=driver.find_element_by_xpath('//*[@id="emailId"]')
-input.send_keys("vkdhoni123@gmail.com") #Replace Your Mail & After OTP Will Come To That Mail You Must Enter That👍
+input.send_keys("yashwanth6678@gmail.com") #replace with your email otp will come you must enter it otp will not fill automatically
 
 input=driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button').click()
 
-#In Last It Ask Select City Select it👍
+time.sleep(2)
+
 
 
 
