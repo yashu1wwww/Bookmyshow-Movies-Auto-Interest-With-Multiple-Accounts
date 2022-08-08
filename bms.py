48cfe1970cf96832b30f473afc6acf6ac943b078
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2022-08-08 14:34:29 +0530

    Update bms.py

--- bms.py
+++ bms.py
@@ -4,16 +4,15 @@ import time
 driver = webdriver.Chrome()
 driver.get("https://in.bookmyshow.com/bengaluru/movies/kranti/ET00323056") #Replace Which Movie You Want To automate Put That Link Here
 
-input=driver.find_element_by_xpath('//*[@id="wzrk-confirm"]').click()
+driver.find_element_by_xpath('//*[@id="wzrk-confirm"]').click()
 
-input=driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section/div[2]/div[2]/button/span').click()
+driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section/div[2]/div[2]/button/span').click()
 
 #time.sleep(4)
 
-input=driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div').click()
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div').click()
 
-input=driver.find_element_by_xpath('//*[@id="emailId"]')
-input.send_keys("yashwanth6678@gmail.com") #replace with your email otp will come you must enter it otp will not fill automatically
+driver.find_element_by_xpath('//*[@id="emailId"]').send_keys("yashwanth6678@gmail.com") #replace with your email otp will come you must enter it otp will not fill automatically
 
 input=driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button').click()
 
