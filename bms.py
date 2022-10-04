79887da153a021af9060e6d28f6b3611183d3fb1
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2022-10-04 13:28:35 +0530

    Update bms.py

--- bms.py
+++ bms.py
@@ -22,6 +22,462 @@ time.sleep(15)
 
 driver.close()
 
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://in.bookmyshow.com/bengaluru/movies/adipurush/ET00137196") #Change To Your Required Movie Url
+
+time.sleep(5)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section/div[2]/div[2]/button/span').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="emailId"]').send_keys("yashwanth6675@gmail.com")
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button').click()
+
+time.sleep(15)
+
+driver.close()
+
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://in.bookmyshow.com/bengaluru/movies/adipurush/ET00137196") #Change To Your Required Movie Url
+
+time.sleep(5)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section/div[2]/div[2]/button/span').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="emailId"]').send_keys("yashwanth6675@gmail.com")
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button').click()
+
+time.sleep(15)
+
+driver.close()
+
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://in.bookmyshow.com/bengaluru/movies/adipurush/ET00137196") #Change To Your Required Movie Url
+
+time.sleep(5)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section/div[2]/div[2]/button/span').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="emailId"]').send_keys("yashwanth6675@gmail.com")
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button').click()
+
+time.sleep(15)
+
+driver.close()
+
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://in.bookmyshow.com/bengaluru/movies/adipurush/ET00137196") #Change To Your Required Movie Url
+
+time.sleep(5)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section/div[2]/div[2]/button/span').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="emailId"]').send_keys("yashwanth6675@gmail.com")
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button').click()
+
+time.sleep(15)
+
+driver.close()
+
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://in.bookmyshow.com/bengaluru/movies/adipurush/ET00137196") #Change To Your Required Movie Url
+
+time.sleep(5)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section/div[2]/div[2]/button/span').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="emailId"]').send_keys("yashwanth6675@gmail.com")
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button').click()
+
+time.sleep(15)
+
+driver.close()
+
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://in.bookmyshow.com/bengaluru/movies/adipurush/ET00137196") #Change To Your Required Movie Url
+
+time.sleep(5)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section/div[2]/div[2]/button/span').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="emailId"]').send_keys("yashwanth6675@gmail.com")
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button').click()
+
+time.sleep(15)
+
+driver.close()
+
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://in.bookmyshow.com/bengaluru/movies/adipurush/ET00137196") #Change To Your Required Movie Url
+
+time.sleep(5)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section/div[2]/div[2]/button/span').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="emailId"]').send_keys("yashwanth6675@gmail.com")
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button').click()
+
+time.sleep(15)
+
+driver.close()
+
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://in.bookmyshow.com/bengaluru/movies/adipurush/ET00137196") #Change To Your Required Movie Url
+
+time.sleep(5)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section/div[2]/div[2]/button/span').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="emailId"]').send_keys("yashwanth6675@gmail.com")
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button').click()
+
+time.sleep(15)
+
+driver.close()
+
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://in.bookmyshow.com/bengaluru/movies/adipurush/ET00137196") #Change To Your Required Movie Url
+
+time.sleep(5)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section/div[2]/div[2]/button/span').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="emailId"]').send_keys("yashwanth6675@gmail.com")
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button').click()
+
+time.sleep(15)
+
+driver.close()
+
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://in.bookmyshow.com/bengaluru/movies/adipurush/ET00137196") #Change To Your Required Movie Url
+
+time.sleep(5)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section/div[2]/div[2]/button/span').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="emailId"]').send_keys("yashwanth6675@gmail.com")
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button').click()
+
+time.sleep(15)
+
+driver.close()
+
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://in.bookmyshow.com/bengaluru/movies/adipurush/ET00137196") #Change To Your Required Movie Url
+
+time.sleep(5)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section/div[2]/div[2]/button/span').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="emailId"]').send_keys("yashwanth6675@gmail.com")
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button').click()
+
+time.sleep(15)
+
+driver.close()
+
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://in.bookmyshow.com/bengaluru/movies/adipurush/ET00137196") #Change To Your Required Movie Url
+
+time.sleep(5)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section/div[2]/div[2]/button/span').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="emailId"]').send_keys("yashwanth6675@gmail.com")
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button').click()
+
+time.sleep(15)
+
+driver.close()
+
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://in.bookmyshow.com/bengaluru/movies/adipurush/ET00137196") #Change To Your Required Movie Url
+
+time.sleep(5)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section/div[2]/div[2]/button/span').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="emailId"]').send_keys("yashwanth6675@gmail.com")
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button').click()
+
+time.sleep(15)
+
+driver.close()
+
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://in.bookmyshow.com/bengaluru/movies/adipurush/ET00137196") #Change To Your Required Movie Url
+
+time.sleep(5)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section/div[2]/div[2]/button/span').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="emailId"]').send_keys("yashwanth6675@gmail.com")
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button').click()
+
+time.sleep(15)
+
+driver.close()
+
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://in.bookmyshow.com/bengaluru/movies/adipurush/ET00137196") #Change To Your Required Movie Url
+
+time.sleep(5)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section/div[2]/div[2]/button/span').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="emailId"]').send_keys("yashwanth6675@gmail.com")
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button').click()
+
+time.sleep(15)
+
+driver.close()
+
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://in.bookmyshow.com/bengaluru/movies/adipurush/ET00137196") #Change To Your Required Movie Url
+
+time.sleep(5)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section/div[2]/div[2]/button/span').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="emailId"]').send_keys("yashwanth6675@gmail.com")
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button').click()
+
+time.sleep(15)
+
+driver.close()
+
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://in.bookmyshow.com/bengaluru/movies/adipurush/ET00137196") #Change To Your Required Movie Url
+
+time.sleep(5)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section/div[2]/div[2]/button/span').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="emailId"]').send_keys("yashwanth6675@gmail.com")
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button').click()
+
+time.sleep(15)
+
+driver.close()
+
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://in.bookmyshow.com/bengaluru/movies/adipurush/ET00137196") #Change To Your Required Movie Url
+
+time.sleep(5)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section/div[2]/div[2]/button/span').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="emailId"]').send_keys("yashwanth6675@gmail.com")
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button').click()
+
+time.sleep(15)
+
+driver.close()
+
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://in.bookmyshow.com/bengaluru/movies/adipurush/ET00137196") #Change To Your Required Movie Url
+
+time.sleep(5)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section/div[2]/div[2]/button/span').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div').click()
+
+time.sleep(4)
+
+driver.find_element_by_xpath('//*[@id="emailId"]').send_keys("yashwanth6675@gmail.com")
+
+driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button').click()
+
+time.sleep(15)
+
+driver.close()
+
 
 
 
